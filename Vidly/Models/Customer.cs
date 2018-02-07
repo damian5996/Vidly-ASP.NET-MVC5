4163cd3fb9a18b92cece0267b00b88bea4695014
Damian Jacyna <135932@student.uwm.edu.pl>
2018-02-07 18:38:07 +0100

    Section 5 - before exercise

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -10,6 +10,7 @@ namespace Vidly.Models
     {
         public int Id { get; set; }
 
+        //[Required(ErrorMessage = "Please enter customer's name.")]
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
@@ -22,6 +23,7 @@ namespace Vidly.Models
         public byte MembershipTypeId { get; set; }
 
         [Display(Name = "Date of Birth")]
+        [Min18YearsIfAMember]
         public DateTime? BirthDate { get; set; }
 
     }
