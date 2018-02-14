04764a9b6a820b44a23f5e41c38678489acb6f53
Damian Jacyna <135932@student.uwm.edu.pl>
2018-02-14 17:45:18 +0100

    Section 6 ( e.g. built an Api for Movies) - after exercise

--- Vidly/ViewModel/CustomerFormViewModel.cs
+++ Vidly/ViewModel/CustomerFormViewModel.cs
@@ -11,16 +11,22 @@ namespace Vidly.ViewModel
         public IEnumerable<MembershipType> MembershipTypes { get; set; }
         public Customer Customer { get; set; }
 
-        public string Title
-        {
-            get
-            {
-                if (Customer != null && Customer.Id != 0)
-                    return "Edit Customer";
+        public string Title  => (Customer != null && Customer.Id != 0) ? "Edit Customer" : "New Customer";
+            
 
-                return "New Customer";
-            }
-        }
+            //get
+            //{
+            //    return (Customer != null && Customer.Id != 0) ? "Edit Customer" : "New Customer";
+            //}
+
+            //get
+            //{
+            //    if (Customer != null && Customer.Id != 0)
+            //        return "Edit Customer";
+
+            //    return "New Customer";
+            //}
+        
 
     }
 }
